c06da110c385ed0a937c233cdd929aa4e58e5202
Arthur Aukhatov <aaukhatov@gmail.com>
2017-10-25 21:28:39 +0500

    update command processing

--- src/main/main.go
+++ src/main/main.go
@@ -11,18 +11,19 @@ import ("log"
 
 const webhook = "https://tat-ru-bot.herokuapp.com/"
 const yandex_api = "dict.1.1.20171024T175215Z.d79c6c40e3a0bf31.0f44341ac31440368c75d3e143c641ab1a7acec6"
+const telegram_token = "384640172:AAFOh_vCuFizDclHRxjpsY0SGoAtlsSCHs4"
 
 func main() {
 	telegram()
 }
 
 func telegram() {
-	log.Print("I'm a bot!")
 	port := os.Getenv("PORT")
-	bot, err := tgbotapi.NewBotAPI("384640172:AAFOh_vCuFizDclHRxjpsY0SGoAtlsSCHs4")
+	bot, err := tgbotapi.NewBotAPI(telegram_token)
 	if err != nil {
 		log.Panic(err)
 	}
+	var userState = make(map[int]string)
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 	_, err = bot.SetWebhook(tgbotapi.NewWebhook(webhook))
@@ -35,26 +36,51 @@ func telegram() {
 	for update := range updates {
 		var msg tgbotapi.MessageConfig
 		log.Printf("Translated request: %s | command %s", update.Message.Text, update.Message.Command())
-		switch update.Message.Command() {
-		case "/rutat":
+
+		var command, err = userState[update.Message.From.ID]
+
+		if err {
+			log.Println("The user not found.")
+			if !update.Message.IsCommand() {
+				log.Println("It's not command")
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Укажите направление перевеода:\n" +
+					"/rutat - русско-татарский\n/tatru - татарско-русский")
+				bot.Send(msg)
+				return
+			}
+			userState[update.Message.From.ID] = update.Message.Command()
+		} else {
+			log.Println("The user found.")
+			if update.Message.Command() != userState[update.Message.From.ID] {
+				log.Println("The user comamnd update.")
+				userState[update.Message.From.ID] = update.Message.Command()
+				command = update.Message.Command()
+			}
+		}
+
+		switch command {
+		case "rutat":
 			inputMsg := strings.Split(update.Message.Text, " ")
 			// берем первое слово всегда
-			translatedWord := translate(inputMsg[1])
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, strings.Join(translatedWord,","))
-
+			translatedWord := translate(inputMsg[0], "ru-tt")
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, strings.Join(translatedWord,", "))
+		case "tatru":
+			inputMsg := strings.Split(update.Message.Text, " ")
+			// берем первое слово всегда
+			translatedWord := translate(inputMsg[0], "tt-ru")
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, strings.Join(translatedWord,", "))
 		default:
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Укажите направление перевода.")
 		}
 
-		msg.ReplyToMessageID = update.Message.MessageID
 		bot.Send(msg)
 	}
 }
 
-func translate(msg string) []string {
+func translate(msg string, dictionary string) []string {
 
 	resp, err := http.Get("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=" +
-		yandex_api + "&lang=ru-tt&text=" + msg)
+		yandex_api + "&lang=" + dictionary + "&text=" + msg)
 
 	if err != nil {
 		log.Println(err)
